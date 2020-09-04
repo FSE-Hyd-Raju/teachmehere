a0f99983ff4489b9bc9ea968da1f29c5a331c018
tagideasteachme <tagideasteachme@gmail.com>
2020-09-05 01:15:06 +0530

    login ui

--- src/screens/tabs/profile/feedbackPage.js
+++ src/screens/tabs/profile/feedbackPage.js
@@ -1,26 +1,37 @@
 import React, { Fragment } from 'react';
 import { View, StyleSheet, ScrollView, Text, TextInput } from 'react-native';
-import { Button, Icon, Avatar } from 'react-native-elements';
-import Theme from '../../../Theme';
+import { Button } from 'react-native-elements';
 import { useDispatch, useSelector } from 'react-redux'
-import { signupSelector } from '../../../redux/slices/signupSlice'
 import PageSpinner from '../../../components/common/PageSpinner';
 import * as yup from 'yup'
 import { Formik } from 'formik'
-import ImagePicker from 'react-native-image-picker';
-import { changeProfileSelector, clearProfileErrors, onChangeProfilePressed } from '../../../redux/slices/changeProfileSlice';
-import { loadUserInfo, loginSelector } from '../../../redux/slices/loginSlice'
+import { changeProfileSelector } from '../../../redux/slices/changeProfileSlice';
+import { loginSelector } from '../../../redux/slices/loginSlice';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import Icons from 'react-native-vector-icons/MaterialCommunityIcons';
+import EmojiRating from '../../../components/common/emojiRating';
+import { Snackbar } from 'react-native-paper';
+
 
 export default function feedbackPage({ navigation }) {
 
     const dispatch = useDispatch()
-    const { DescriptionError, loading } = useSelector(changeProfileSelector);
+    const { DescriptionError } = useSelector(changeProfileSelector);
+    const [feedbackRating, setFeedbackRating] = React.useState("GOOD");
+    const [loading, setLoading] = React.useState(false);
+    const [visibleSnackbar, setVisibleSnackbar] = React.useState("");
+    const { userInfo } = useSelector(loginSelector);
 
 
     const descriptionComponent = () => {
-
         const onsubmit = (values) => {
-
+            var obj = {
+                userId: userInfo ? userInfo._id : null,
+                userName: userInfo ? userInfo.username : null,
+                feedbackRating: feedbackRating,
+                description: values.description
+            }
+            submitFeedback(obj);
         }
         return (
             <Formik
@@ -46,7 +57,7 @@ export default function feedbackPage({ navigation }) {
                                         borderBottomColor: '#000000',
                                         borderBottomWidth: 1,
                                     }}
-                                    placeholder="Tell others about yourself"
+                                    placeholder="Tell us more... "
                                     multiline
                                     numberOfLines={4}
                                     errorMessage={DescriptionError}
@@ -73,28 +84,90 @@ export default function feedbackPage({ navigation }) {
         );
     };
 
+    const submitFeedback = (obj) => {
+        setLoading(true);
+        fetch('https://teachmeproject.herokuapp.com/submitfeedback', {
+            method: 'POST',
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(obj)
+        }).then((responseJson) => {
+            setVisibleSnackbar("Feedback sent successfully!")
+            setLoading(false);
+            navigation.goBack();
+        }).catch((error) => {
+            setVisibleSnackbar("Something went wrong!")
+            console.error(error);
+            setLoading(false);
+        });
+    }
+
+    const snackComponent = () => {
+        return (
+            <Snackbar
+                visible={!!visibleSnackbar}
+                onDismiss={() => setVisibleSnackbar("")}
+                duration={2000}
+                action={{
+                    label: 'Dismiss',
+                    onPress: () => {
+                        setVisibleSnackbar("")
+                    },
+                }}
+                style={{ backgroundColor: "white" }}
+                wrapperStyle={{ backgroundColor: "white" }}
+            >
+                <Text style={{ color: "black", fontSize: 16, letterSpacing: 1 }}>{visibleSnackbar}</Text>
+            </Snackbar>
+        )
+    }
+
     const headerComponent = () => {
         return (
-            <View style={{
-                justifyContent: "center",
-                alignItems: "center",
-                // marginTop: 20,
-                marginBottom: 50,
-            }}>
-                <Text style={{ fontSize: 20, color: "black", letterSpacing: 1, fontWeight: "bold" }}>Submit your feedbackPage</Text>
+            <View>
+                <View style={{
+                    flexDirection: "row",
+                    alignItems: "center",
+                    // marginTop: 20,
+                    marginBottom: 35,
+                }}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <Icons
+                            name={"keyboard-backspace"}
+                            size={27}
+                            color={"grey"}
+                        />
+                    </TouchableOpacity>
+                    <View style={{ justifyContent: "flex-end", flex: 0.9, marginLeft: 10 }}>
+                        <Text style={{ fontSize: 20, color: "black", letterSpacing: 1, textAlign: "center" }}>Share your feedback</Text>
+                    </View>
+                </View>
+                {/* <Text style={{ fontSize: 14, color: "grey", textAlign: "center" }}>Provide your feedback and help us serve you better.</Text> */}
+
             </View>
         )
     }
 
+    const feedbackValue = (value) => {
+        console.log(value);
+        setFeedbackRating(value)
+    }
+
     return (
         <View style={styles.MainContainer}>
-            <ScrollView showsVerticalScrollIndicator={false} keyboardShouldPersistTaps={"handled"}>
+            <ScrollView showsVerticalScrollIndicator={false} keyboardShouldPersistTaps={"handled"} style={{ padding: 30 }}>
                 <View>
                     {headerComponent()}
+                    <View style={{ marginVertical: 45 }}>
+                        <EmojiRating feedbackValue={(value) => feedbackValue(value)}></EmojiRating>
+                    </View>
                     {descriptionComponent()}
                 </View>
                 <PageSpinner visible={loading} />
             </ScrollView >
+            {snackComponent()}
         </View>
     );
 }
@@ -102,12 +175,11 @@ export default function feedbackPage({ navigation }) {
 const styles = StyleSheet.create({
     MainContainer: {
         flex: 1,
-        padding: 30,
+        // padding: 30,
         backgroundColor: "rgb(255, 255, 255)"
     },
     loginButton: {
         alignSelf: 'center',
-        // marginVertical: Theme.spacing.small,
         marginVertical: 35,
         width: 180,
         borderRadius: 20
